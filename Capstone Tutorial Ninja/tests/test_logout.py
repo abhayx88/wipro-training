05e8f411d0aa04a498f6f66a28e9053329aa9ac4
abhayx88 <97593611+abhayx88@users.noreply.github.com>
2026-02-17 21:38:57 +0530

    Update test_logout.py

--- Capstone Tutorial Ninja/tests/test_logout.py
+++ Capstone Tutorial Ninja/tests/test_logout.py
@@ -1,14 +1,32 @@
+import csv
+import os
+import pytest
 from pages.login_page import LoginPage
 from pages.account_page import AccountPage
 
 
+def get_first_user():
+    base = os.path.dirname(os.path.dirname(__file__))
+    path = os.path.join(base, "data", "users.csv")
+
+    with open(path, newline="") as f:
+        reader = csv.DictReader(f)
+        return next(reader)
+
+@pytest.mark.smoke
+@pytest.mark.order(6)
 def test_logout(driver):
 
+    user = get_first_user()
+
+    # LOGIN
     login = LoginPage(driver)
     login.open_login()
-    login.login("abhay+1@gmail.com", "Test@123")   # use your password
+    login.login(user["email"], user["password"])
 
+    # LOGOUT
     account = AccountPage(driver)
     account.logout()
 
+    # VERIFY LOGOUT PAGE
     assert "Account Logout" in driver.page_source

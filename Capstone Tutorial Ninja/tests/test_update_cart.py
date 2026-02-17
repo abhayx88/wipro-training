32311a94be2c9b6c917329e4ddb49314e52d5175
abhayx88 <97593611+abhayx88@users.noreply.github.com>
2026-02-17 21:41:06 +0530

    Update test_update_cart.py

--- Capstone Tutorial Ninja/tests/test_update_cart.py
+++ Capstone Tutorial Ninja/tests/test_update_cart.py
@@ -1,22 +1,41 @@
+import pytest
+import csv
+import os
 from pages.search_page import SearchPage
 from pages.product_page import ProductPage
 from pages.cart_page import CartPage
 
 
-def test_update_and_remove_cart(driver):
+def load_products():
+    base = os.path.dirname(os.path.dirname(__file__))
+    path = os.path.join(base, "data", "products.csv")
 
+    with open(path, newline="") as f:
+        return [row["product"] for row in csv.DictReader(f)]
+
+
+@pytest.mark.smoke
+@pytest.mark.order(5)
+@pytest.mark.parametrize("product_name", load_products())
+def test_update_and_remove_cart(driver, product_name):
+
+    # SEARCH
     search = SearchPage(driver)
-    search.search_product("iphone")
+    search.search_product(product_name)
     search.open_first_product()
 
+    # ADD
     product = ProductPage(driver)
     product.add_to_cart()
 
+    # OPEN CART
     cart = CartPage(driver)
     cart.open_cart()
 
+    # UPDATE
     cart.update_quantity("2")
 
+    # REMOVE
     cart.remove_item()
 
     assert "Shopping Cart" in driver.title

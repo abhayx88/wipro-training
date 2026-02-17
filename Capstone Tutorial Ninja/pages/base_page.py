9dfb82162becc9b591e873fae24f8100b6e50f2d
abhayx88 <97593611+abhayx88@users.noreply.github.com>
2026-02-17 21:30:10 +0530

    Update base_page.py

--- Capstone Tutorial Ninja/pages/base_page.py
+++ Capstone Tutorial Ninja/pages/base_page.py
@@ -12,26 +12,27 @@ class BasePage:
         self.wait = WebDriverWait(driver, get_explicit_wait())
 
     def find(self, locator):
-        return self.wait.until(
-            EC.presence_of_element_located(locator)
-        )
+        return self.wait.until(EC.presence_of_element_located(locator))
 
     def click(self, locator):
         for _ in range(3):
             try:
-                self.wait.until(
+                element = self.wait.until(
                     EC.element_to_be_clickable(locator)
-                ).click()
-                time.sleep(1.5)   # PERFECT DEMO SPEED
+                )
+                element.click()
+                time.sleep(1.2)   # demo visible speed
                 return
             except StaleElementReferenceException:
-                pass
+                time.sleep(1)
+
+        raise Exception("Element not clickable")
 
     def type(self, locator, text):
         element = self.find(locator)
         element.clear()
         element.send_keys(text)
-        time.sleep(1.5)
+        time.sleep(1.2)
 
     def get_text(self, locator):
         return self.find(locator).text

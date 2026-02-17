716241e4e585cd5063efddbec9db209b67d56876
abhayx88 <97593611+abhayx88@users.noreply.github.com>
2026-02-17 21:38:00 +0530

    Update test_login.py

--- Capstone Tutorial Ninja/tests/test_login.py
+++ Capstone Tutorial Ninja/tests/test_login.py
@@ -1,12 +1,25 @@
+import csv
+import os
+import pytest
 from pages.login_page import LoginPage
 
 
+def get_first_user():
+    base = os.path.dirname(os.path.dirname(__file__))
+    path = os.path.join(base, "data", "users.csv")
+
+    with open(path, newline="") as f:
+        reader = csv.DictReader(f)
+        return next(reader)   # ONLY first user
+
+@pytest.mark.smoke
+@pytest.mark.order(2)
 def test_login(driver):
 
+    user = get_first_user()
+
     login = LoginPage(driver)
-
     login.open_login()
-
-    login.login("abhay+1@gmail.com", "Test@123")   # ← use your real password here
+    login.login(user["email"], user["password"])
 
     assert "My Account" in driver.title
